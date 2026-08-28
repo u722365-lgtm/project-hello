459fea08be095f85350622a8831ab299a3f2c3bb
zain836 <zaima2523@gmail.com>
2026-08-28 20:14:45 +0500

    Fix Vite HMR issue with TooltipProvider

--- src/components/ui/tooltip.tsx
+++ src/components/ui/tooltip.tsx
@@ -3,7 +3,9 @@ import * as TooltipPrimitive from "@radix-ui/react-tooltip";
 
 import { cn } from "@/lib/utils";
 
-const TooltipProvider = TooltipPrimitive.Provider;
+const TooltipProvider = ({ children, ...props }: React.ComponentProps<typeof TooltipPrimitive.Provider>) => (
+  <TooltipPrimitive.Provider {...props}>{children}</TooltipPrimitive.Provider>
+);
 
 const Tooltip = TooltipPrimitive.Root;
 
